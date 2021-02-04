038f9cea82df718d8cf142c4f51e102df3e0be6b
EastQ4 <76393895+EastQ4@users.noreply.github.com>
2021-02-04 21:36:24 +0300

    Add files via upload

---
+++ adoncia.js
@@ -0,0 +1,211 @@
+const Discord = require('discord.js');//
+const client = new Discord.Client();//
+const ayarlar = require('./ayarlar.json');//
+const chalk = require('chalk');//
+const moment = require('moment');//
+var Jimp = require('jimp');//
+const { Client, Util } = require('discord.js');//
+const fs = require('fs');//
+const db = require('quick.db');//
+const express = require('express');//
+require('./util/eventLoader.js')(client);//
+const path = require('path');//
+const snekfetch = require('snekfetch');//
+//
+
+var prefix = ayarlar.prefix;//
+//
+const log = message => {//
+    console.log(`${message}`);//
+};
+
+client.commands = new Discord.Collection();//
+client.aliases = new Discord.Collection();//
+fs.readdir('./komutlar/', (err, files) => {//
+    if (err) console.error(err);//
+    log(`${files.length} komut yüklenecek.`);//
+    files.forEach(f => {//
+        let props = require(`./komutlar/${f}`);//
+        log(`Yüklenen komut: ${props.help.name}.`);//
+        client.commands.set(props.help.name, props);//
+        props.conf.aliases.forEach(alias => {//
+            client.aliases.set(alias, props.help.name);//
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+
+
+//-----------------------GİRENE-ROL-VERME----------------------\\     
+let kayıtsız = ayarlar.kayıtsızROL
+let kayıtısz = ayarlar.kayıtsızROL2
+
+client.on("guildMemberAdd", member => {
+  member.setNickname(`• İsim | Yaş`)
+  member.roles.add(kayıtsız);
+  member.roles.add(kayıtısz)
+});
+
+//-----------------------GİRENE-ROL-VERME SON----------------------\\     
+
+
+
+
+//-----------------------HOŞ-GELDİN-MESAJI----------------------\\     
+
+client.on("guildMemberAdd", member => {
+    require("moment-duration-format")
+      var üyesayısı = member.guild.members.cache.size.toString().replace(/ /g, "    ")
+      var üs = üyesayısı.match(/([0-999])/g)
+      üyesayısı = üyesayısı.replace(/([a-zA-Z])/g, "bilinmiyor").toLowerCase()
+      if(üs) {
+        üyesayısı = üyesayısı.replace(/([0-9999])/g, d => {
+          return {
+            '0': `<a:revolts_0:777236510771773440>`,
+            '1': `<a:revolts_1:777236093900161054>`,
+            '2': `<a:revolts_2:777236113902665810>`,
+            '3': `<a:revolts_3:777236141224230952>`,
+            '4': `<a:revolts_4:777236167761592390>`,                       
+            '5': `<a:revolts_5:777236339841957899>`,
+            '6': `<a:revolts_6:777236384738050068>`,
+            '7': `<a:revolts_7:777236413921755166>`,
+            '8': `<a:revolts_8:777236444103704576>`,
+            '9': `<a:revolts_9:777236474009485352>`}[d];})}
+    const kanal = member.guild.channels.cache.find(r => r.id === "806158601927655477");
+    let user = client.users.cache.get(member.id);
+    require("moment-duration-format");
+      const kurulus = new Date().getTime() - user.createdAt.getTime();  
+     const gecen = moment.duration(kurulus).format(` YY **[Yıl,]** DD **[Gün,]** HH **[Saat,]** mm **[Dakika,]** ss **[Saniye]**`) 
+    var kontrol;
+  if (kurulus < 1296000000) kontrol = 'Güvenilir Gözükmüyor.'
+  if (kurulus > 1296000000) kontrol = 'Hesap Güvenli'
+    moment.locale("tr");
+    kanal.send(`
+**S İ L L A G E HOŞGELDİNİZ
+
+Hoşgeldin <@`+ member + `> Seninle ${member.guild.memberCount} Kişiyiz
+
+Sunucuya Kayıt Olmak İçin Sol Taraftaki #Voice Confirmed Odalarına Geçiş Yapabilirsin     
+
+Bu Roldeki Arkadaşlarım Seninle İlgilenecektir  <@&806158601524740143> 
+
+`+kontrol+`**`)});
+
+  
+//-----------------------HOŞ-GELDİN-MESAJI SON----------------------\\     
+
+
+//-----------------------TAG-ROL-LOG----------------------\\  
+client.on("userUpdate", async (oldUser, newUser) => {
+  if (oldUser.username !== newUser.username) {
+  const tag = '♱'
+  const sunucu = '806158601223143434'
+  const log = '806568579851485234'
+  const rol = '806158601478733866'
+
+  try {
+
+  if (newUser.username.includes(tag) && !client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+  await client.channels.cache.get(log).send(new Discord.MessageEmbed().setColor("GREEN").setDescription(`${newUser} ${tag} Tagımızı Aldığı İçin <@&${rol}> Rolünü Verdim`));
+  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.add(rol);
+  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).send(`Selam ${newUser.username}, Sunucumuzda ${tag} Tagımızı Aldığın İçin ${client.guilds.cache.get(sunucu).roles.cache.get(rol).name} Rolünü Sana Verdim!`)
+  }
+  if (!newUser.username.includes(tag) && client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+  await client.channels.cache.get(log).send(new Discord.MessageEmbed().setColor("RED").setDescription(`${newUser} ${tag} Tagımızı Çıkardığı İçin <@&${rol}> Rolünü Aldım`));
+  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.remove(rol);
+  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).send(`Selam **${newUser.username}**, Sunucumuzda ${tag} Tagımızı Çıkardığın İçin ${client.guilds.cache.get(sunucu).roles.cache.get(rol).name} Rolünü Senden Aldım!`)
+  }
+} catch (e) {
+console.log(`Bir hata oluştu! ${e}`)
+ }
+}
+});
+//-----------------------TAG-ROL-LOG-SON----------------------\\  
+
+client.on("ready", async function() {
+const voiceChannel = "806566070336880640"
+client.channels.cache.get(voiceChannel).join()
+.catch(err => {
+throw err;
+})
+})
